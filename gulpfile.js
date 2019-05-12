b2f2e33d51cf5b2b9885866c2867b9bad4dc9b91
marcy <141490+marchica@users.noreply.github.com>
2019-05-12 01:41:51 -0400

    Add gulp task to start Slack

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const { src, dest, watch, series } = require('gulp');
 const del = require('del');
 const connect = require('gulp-connect');
+const exec = require('child_process').exec;
 
 let config = {
     paths: {
@@ -26,13 +27,23 @@ function server(cb) {
     cb();
 }
 
+function startSlack(cb) {
+    //TODO - detect latest version
+    exec('set SLACK_DEVELOPER_MENU=true && %LocalAppData%\\slack\\app-3.4.0\\slack.exe', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+    cb();
+}
+
 function css(cb) {
     src(config.paths.input)
         .pipe(dest(config.paths.output));
     cb();
 }
 
-function watch(cb) {
+function watcher(cb) {
     watch([config.paths.input], css);
     cb();
 }
@@ -48,4 +59,5 @@ function build(cb) {
 
 exports.build = build;
 exports.clean = clean;
-exports.default = series(clean, build, server, watch);
+exports.startSlack = startSlack;
+exports.default = series(clean, build, server, startSlack, watcher);
