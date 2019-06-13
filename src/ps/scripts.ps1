2782915cb266bb8d84332aecfb36befb023cd973
Cromzinc <Cromzinc@gmail.com>
2019-06-13 14:13:54 -0400

    Update scripts.ps1

--- src/ps/scripts.ps1
+++ src/ps/scripts.ps1
@@ -17,41 +17,40 @@ function InstallSlackPatch([switch] $DevMode = $false) {
 
     # Backup original files
     if ((Test-Path -Path "$slackFile.bak") -eq $False) {
-        Copy-Item -Path $slackFile -Destination "$slackFile.bak"
     }
 
     # Read slack file into memory
     $fileContents = Get-Content $slackFile
 
     # Check if files have already been patched
-    $patchIdentifier = "//Patch from https://github.com/marchica/slack-black-theme"
+    $patchIdentifier = '//Patch from https://github.com/marchica/slack-black-theme'
 
     if ($fileContents | Select-String -Pattern $patchIdentifier -SimpleMatch -Quiet) {
-        Write-Host "Already patched!"
+        Write-Host 'Already patched!'
         exit
     }
 
     Write-Host "Patching $slackFile..."
 
     # Read patch into memory and replace URL
-    $urlPlaceholder = "URL_TO_CSS"
+    $urlPlaceholder = 'URL_TO_CSS'
     if ($DevMode) {
-        $url = "http://127.0.0.1:8080/custom.css"
+        $url = 'http://127.0.0.1:8080/custom.css'
     } else {
-        $url = "https://raw.githubusercontent.com/marchica/slack-black-theme/master/dist/custom.css";
+        $url = 'https://raw.githubusercontent.com/marchica/slack-black-theme/master/dist/custom.css';
     }
 
-    $patchContents = (Invoke-WebRequest "https://raw.githubusercontent.com/marchica/slack-black-theme/master/src/js/SlackPatch.js" -UseBasicParsing).Content.Replace($urlPlaceholder, $url)
+    $patchContents = (Invoke-WebRequest 'https://raw.githubusercontent.com/marchica/slack-black-theme/master/src/js/SlackPatch.js' -UseBasicParsing).Content.Replace($urlPlaceholder, $url)
 
     # Add patch to end of slack file
     Add-Content -Path $slackFile -Value $patchContents
 
     # If dev mode, add dev patch to auto-reload CSS
     if ($DevMode) {
-        $pathPlaceholder = "PATH_TO_LOCAL_CSS"
+        $pathPlaceholder = 'PATH_TO_LOCAL_CSS'
         $path = (Join-Path (Resolve-Path $PSScriptRoot\..\..) dist\custom.css).Replace('\', '\\')
 
-        $devPatchContents = (Invoke-WebRequest "https://raw.githubusercontent.com/marchica/slack-black-theme/master/src/js/DevSlackPatch.js" -UseBasicParsing).Content.Replace($pathPlaceholder, $path)
+        $devPatchContents = (Invoke-WebRequest 'https://raw.githubusercontent.com/marchica/slack-black-theme/master/src/js/DevSlackPatch.js' -UseBasicParsing).Content.Replace($pathPlaceholder, $path)
 
         # Add patch to end of slack file
         Add-Content -Path $slackFile -Value $devPatchContents
