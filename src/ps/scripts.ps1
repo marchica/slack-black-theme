a99f02a2a2d88b12fa217cf7472f6947a9bbca36
marcy <141490+marchica@users.noreply.github.com>
2019-07-05 21:43:22 -0400

    Add custom transparent Slack icon patcher

--- src/ps/scripts.ps1
+++ src/ps/scripts.ps1
@@ -114,3 +114,33 @@ function Uninstall-SlackPatch() {
         Move-Item -Path $slackFolder\ssb-interop.js.bak -Destination $slackFolder\ssb-interop.js -Force
     }
 }
+
+function Install-SlackIconPatch() {
+    $slackBasePath = Get-SlackFolder
+
+    if (!$slackBasePath) {
+        return
+    }
+
+    # TODO - download
+    if ((Test-Path -Path $slackBasePath\slack-logo-transparent.ico) -eq $False) {
+        Copy-Item -Path $PSScriptRoot\..\..\resources\slack-logo-transparent.ico -Destination $slackBasePath
+    }
+
+    $startMenuPath = [Environment]::GetFolderPath('StartMenu')
+    $slackShortcut = Join-Path $startMenuPath 'Programs\Slack Technologies\Slack.lnk'
+
+    if ((Test-Path -Path $slackShortcut) -eq $False) {
+        [Console]::ForegroundColor = 'red'
+        [Console]::Error.WriteLine('Unable to locate Slack''s Start Menu shortcut')
+        [Console]::ResetColor()
+        return
+    }
+
+    $shell = New-Object -COM WScript.Shell
+    $shortcut = $shell.CreateShortcut($slackShortcut)
+    $shortcut.IconLocation = "$slackBasePath\slack-logo-transparent.ico"
+    $shortcut.Save()
+
+    Write-Output 'Successfully patched Slack''s icon!'
+}
