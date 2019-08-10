8e922e39437bf0e712faf673a04353c5a8937320
marcy <141490+marchica@users.noreply.github.com>
2019-08-10 01:04:53 -0400

    Add eslint and work on new node script

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const { src, dest, watch, series, parallel } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const c = require('ansi-colors');
 const connect = require('gulp-connect');
 const del = require('del');
 const eslint = require('gulp-eslint');
 const log = require('fancy-log');
-const shell = require('node-powershell');
+
+const slackPatcher = require('./src/js/SlackPatcher');
+
 
 let config = {
     paths: {
         /* Input */
         cssFiles: './src/css/*.css',
         gulpFile: './gulpfile.js',
-        jsFiles: './src/js/*.js',
-        powerShellFiles: './src/ps/*.ps1',
-        powerShellScript: '.\\src\\ps\\scripts.ps1',
+        jsFiles: './src/js/SlackPatcher.js', //TODO: include all?
 
         /* Output */
         output: './dist/'
@@ -23,7 +23,7 @@ let config = {
 };
 
 function lint() {
-    return gulp.src([config.paths.gulpFile, config.paths.jsFiles])
+    return src([config.paths.gulpFile, config.paths.jsFiles])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
@@ -44,84 +44,45 @@ function server(cb) {
     cb();
 }
 
-function startSlack() {
+async function startSlack() {
     log.info('Launching Slack in developer mode');
-    return runPowerShellScript(`. ${config.paths.powerShellScript}; Invoke-SlackDevMode`, printDevInfo);
-}
-
-function printDevInfo() {
+    slackPatcher.launchSlack();
     log.info(c.bold.magenta('** Ctrl-Alt-I to open dev tools in Slack **'));
     log.info(c.bold.magenta('** Ctrl-R to refresh Slack after CSS changes **'));
+    await Promise.resolve();
 }
 
 function installSlackPatch() {
     log.info('Installing Slack patch');
-    return runPowerShellScript(`. ${config.paths.powerShellScript}; Install-SlackPatch -DevMode`);
+    //Install-SlackPatch -DevMode`);
 }
 
 function uninstallSlackPatch() {
     log.info('Uninstalling Slack patch');
-    return runPowerShellScript(`. ${config.paths.powerShellScript}; Uninstall-SlackPatch`);
+    // Uninstall-SlackPatch`);
 }
 
-function runPSScriptAnalyzer() {
-    log.info('Running PSScriptAnalyzer');
-    // If this is throwing an error, make sure it's installed: Install-Module -Name PSScriptAnalyzer
-    return runPowerShellScript(`Invoke-ScriptAnalyzer -Path ${config.paths.powerShellFiles}`, formatPSScriptAnalyzerResults);
-}
-
-function formatPSScriptAnalyzerResults(output) {
-    if (output)
-        log.error(c.bold.red(output));
-}
-
-function runPSTests() {
-    log.info('Running PowerShell tests');
-    return runPowerShellScript('Invoke-Pester -PassThru -Show None | Select-Object -ExpandProperty TestResult | ConvertTo-Json -Compress', formatPSTestResults);
-}
-
-function formatPSTestResults(output) {
-    let tests = JSON.parse(output);
+// function formatPSTestResults(output) {
+//     let tests = JSON.parse(output);
     
-    if (!Array.isArray(tests))
-        tests = new Array(tests);
+//     if (!Array.isArray(tests))
+//         tests = new Array(tests);
 
-    const failures = tests.filter(test => !test.Passed);
-    const totalTests = tests.length;
-    const failedTests = failures.length;
-    const passedTests = totalTests - failedTests;
+//     const failures = tests.filter(test => !test.Passed);
+//     const totalTests = tests.length;
+//     const failedTests = failures.length;
+//     const passedTests = totalTests - failedTests;
 
-    failures.forEach(test => {
-        log.error(c.bold.red(`Failed test: ${test.Name}`));
-        log.error(test.FailureMessage);
-    });
+//     failures.forEach(test => {
+//         log.error(c.bold.red(`Failed test: ${test.Name}`));
+//         log.error(test.FailureMessage);
+//     });
 
-    log(c.cyan(`${passedTests} of ${totalTests} passed. `) + (failedTests ? c.red(`${failedTests} failed.`) : ''));
+//     log(c.cyan(`${passedTests} of ${totalTests} passed. `) + (failedTests ? c.red(`${failedTests} failed.`) : ''));
 
-    if (totalTests === passedTests)
-        log(c.green('ALL TESTS PASSED!  \\(ᵔᵕᵔ)/'));
-}
-
-function runPowerShellScript(command, cb) {
-    let ps = new shell({
-        executionPolicy: 'Bypass',
-        noProfile: true
-    });
-    ps.addCommand(command);
-    return ps.invoke()
-        .then(output => {
-            if (cb)
-                cb(output);
-            else if (output) 
-                log(output);
-        })
-        .catch(err => {
-            log.error(err);
-        })
-        .then(() => {
-            ps.dispose();
-        });
-}
+//     if (totalTests === passedTests)
+//         log(c.green('ALL TESTS PASSED!  \\(ᵔᵕᵔ)/'));
+// }
 
 function css() {
     return src(config.paths.cssFiles)
@@ -130,7 +91,7 @@ function css() {
 
 function watcher() {
     watch([config.paths.cssFiles], css);
-    watch([config.paths.powerShellFiles], parallel(runPSScriptAnalyzer, runPSTests));
+    watch([config.paths.gulpFile, config.paths.jsFiles], lint);
 }
 
 function clean() {
@@ -147,6 +108,4 @@ exports.lint = lint;
 exports.startSlack = startSlack;
 exports.installSlackPatch = installSlackPatch;
 exports.uninstallSlackPatch = uninstallSlackPatch;
-exports.runPSScriptAnalyzer = runPSScriptAnalyzer;
-exports.runTests = runPSTests;
 exports.default = series(clean, build, server, startSlack, watcher);
--- src/js/SlackPatcher.js
+++ src/js/SlackPatcher.js
@@ -1,7 +1,12 @@
+'use strict';
+
 switch(process.argv[2].toLowerCase()) {
     case 'findlatestslackfolder':
         findLatestSlackFolder();
         break;
+    case 'launchslack':
+        launchSlack();
+        break; 
     case 'installslackpatch':
         installSlackPatch();
         break;
@@ -10,20 +15,51 @@ switch(process.argv[2].toLowerCase()) {
 }
 
 function findLatestSlackFolder() {
-    const fs = require('fs')
-    const { join } = require('path')
+    const fs = require('fs');
+    const { join } = require('path');
+    const semver = require('semver');
 
     const getAppFolders = path => fs.readdirSync(path).filter(folder => fs.statSync(join(path, folder)).isDirectory() && folder.startsWith('app-'))
 
     let slackPath = join(process.env['LOCALAPPDATA'], 'Slack');
-    console.log(slackPath);
-    fs.exists(slackPath, (exists) => {
-        if (exists)
-            console.log(getAppFolders(slackPath));
 
+    fs.exists(slackPath, (exists) => {
+        if (exists){
+            let versions = getAppFolders(slackPath).map(folder => folder.replace('app-', ''));
+            let latestVersion = '0.0.0';
+            versions.forEach(version => {
+                if (semver.gt(version, latestVersion))
+                    latestVersion = version;
+            });
+            slackPath = join(slackPath, `app-${latestVersion}`);
+            
+        }
     });
+    console.log(slackPath);
+    return slackPath;
 }
 
 function installSlackPatch() {
     let devMode = !!(process.argv[3] && process.argv[3].toLowerCase() === '-devmode');
+    console.log(`DevMode: ${devMode}`)
 }
+
+function launchSlack() {
+    var exec = require('child_process').execFile;
+    const { join } = require('path');
+
+    const slackBasePath = findLatestSlackFolder(); //TODO - should this include version folder?
+
+    if (!slackBasePath) {
+        return;
+    }
+
+    process.env['SLACK_DEVELOPER_MENU'] = true;
+
+    exec(join(slackBasePath, 'slack.exe'), function(err, data) {  
+        console.log(err)
+        console.log(data.toString());                       
+    });
+}
+
+exports.launchSlack = launchSlack;
